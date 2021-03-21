5b012a903714d3e4549f645e2bf0f97358a7feae
shahriarkasib <shahriarsourav@iut-dhaka.edu>
2021-03-21 22:27:07 +0600

    test added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Quality predictor package",
+    author="sourav",
+    packages=find_packages(),
+    license="MIT"
+)
