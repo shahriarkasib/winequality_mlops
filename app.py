8646614938a310103af4ab1b1652aaa4b9007d10
shahriarkasib <shahriarsourav@iut-dhaka.edu>
2021-03-23 01:26:52 +0600

    app created and tested

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import pandas as pd
+import joblib
+import numpy as np
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
+
+app1 = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response": response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {error: "Something went wrong!! Try again"}
+        return error
+
+
+
+@app1.route("/", methods = ["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response = predict(data)
+                return render_template ("index.html", response = response)
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+        except Exception as e:
+            error = {error:"Someting went wrong!! Try again"}
+            return render_template("404.html", error = error)
+    else:
+        return render_template("index.html")
+
+if __name__=="__main__":
+    app1.run(host = '127.0.0.1',port = 8000, debug = True)
